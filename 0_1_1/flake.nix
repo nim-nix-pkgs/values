72300215510637e7ae716e0baa62e2abd1c9f879
hugosenari <hugosenari@gmail.com>
2022-03-22 00:38:58 -0300

    chore: re-index ../nimpkgs/v/values

--- 0_1_1/flake.nix
+++ 0_1_1/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-values-0_1_1.flake = false;
   inputs.src-values-0_1_1.owner = "nim-appkit";
-  inputs.src-values-0_1_1.ref   = "refs/tags/0.1.1";
+  inputs.src-values-0_1_1.ref   = "0_1_1";
   inputs.src-values-0_1_1.repo  = "values";
   inputs.src-values-0_1_1.type  = "github";
   
